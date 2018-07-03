c1a3e48dcc855c6b7862d32265d45f490aa7d7f3
hidoos <sk8huaban@gmail.com>
2018-07-03 17:19:51 +0800

    拆分嵌套的 for loop 为独立的 for loop，针对单个数据做处理： 1. 使用单个 for loop 2. 使用 pipeline methods 处理

--- initial-store.js
+++ initial-store.js
@@ -2,54 +2,82 @@ import { movies } from './movies';
 import { customer } from "./customer";
 
 function statement(customer, movies) {
-  let totalAmount = 0;
-  let frequentRenterPoints = 0;
   let result = `Rental Record for ${customer.name}\n`;
+
   for (let r of customer.rentals) {
-    frequentRenterPoints += frequentRenterPointsFor(r);
-    //print figures for this rental
     result += `\t${movieFor(r).title}\t${amountFor(r)}\n`;
-    totalAmount += amountFor(r);
   }
+
   // add footer lines
-  result += `Amount owed is ${totalAmount}\n`;
-  result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+  result += `Amount owed is ${totalAmount()}\n`;
+  result += `You earned ${totalFrequentRenterPoints()} frequent renter points\n`;
 
-  return result;
-}
-
-function amountFor(r) {
-  let result = 0;
-
-  // determine amount for each movie
-  switch (movieFor(r).code) {
-    case 'regular':
-      result = 2;
-      if (r.days > 2) {
-        result += (r.days - 2) * 1.5;
-      }
-      break;
-    case 'new':
-      result = r.days * 3;
-      break;
-    case 'childrens':
-      result = 1.5;
-      if (r.days > 3) {
-        result += (r.days - 3) * 1.5;
-      }
-      break;
+  function totalAmount() {
+    let result = 0;
+    for (let r of customer.rentals) {
+      result += amountFor(r);
+    }
+    return result;
   }
+
+  function totalFrequentRenterPoints() {
+    let result = 0;
+    for (let r of customer.rentals) {
+      result += frequentRenterPointsFor(r);
+    }
+    return result;
+  }
+
+  /**
+   * another way is using pipeline methods
+
+   function totalAmountPipe() {
+      return customer.rentals
+        .reduce((total, r) => total + amountFor(r), 0);
+    }
+
+   function totalFrequentRenterPointsPipe() {
+      return customer.rentals
+        .map(r => frequentRenterPointsFor(r))
+        .reduce((a, b) => a + b, 0)
+    }
+   */
+
+  function movieFor(r) {
+    return movies[r.movieID];
+  }
+
+  function amountFor(r) {
+    let result = 0;
+
+    // determine amount for each movie
+    switch (movieFor(r).code) {
+      case 'regular':
+        result = 2;
+        if (r.days > 2) {
+          result += (r.days - 2) * 1.5;
+        }
+        break;
+      case 'new':
+        result = r.days * 3;
+        break;
+      case 'childrens':
+        result = 1.5;
+        if (r.days > 3) {
+          result += (r.days - 3) * 1.5;
+        }
+        break;
+    }
+    return result;
+  }
+
+  function frequentRenterPointsFor(r) {
+    // add frequent renter points
+    // add bonus for a two day new release rental
+    return (movieFor(r).code === 'new' && r.days > 2) ? 2 : 1;
+  }
+
   return result;
-}
-
-function movieFor(r) {
-  return movies[r.movieID];
-}
-
-function frequentRenterPointsFor(r) {
-  // add frequent renter points
-  // add bonus for a two day new release rental
-  return (movieFor(r).code === 'new' && r.days > 2) ? 2 : 1;
 }
 
 statement(customer, movies);
