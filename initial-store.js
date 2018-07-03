6f559f30719d9904b0bb6dccfe37733d8c71c458
hidoos <sk8huaban@gmail.com>
2018-07-03 15:19:17 +0800

    增加 statement 函数的期望输出，以及为什么重构

--- initial-store.js
+++ initial-store.js
@@ -40,3 +40,21 @@ function statement(customer, movies) {
 
   return result;
 }
+
+/* statement method output 
+  Rental Record for martin
+    Ran 3.5
+    Trois Couleurs: Bleu 2
+  Amount owed is 5.5
+  You earned 2 frequent renter points
+*/
+
+/* hope output 
+  <h1>Rental Record for <em>martin</em></h1>
+  <table>
+    <tr><td>Ran</td><td>3.5</td></tr>
+    <tr><td>Trois Couleurs: Bleu</td><td>2</td></tr>
+  </table>
+  <p>Amount owed is <em>5.5</em></p>
+  <p>You earned <em>2</em> frequent renter points</p>
+ */
