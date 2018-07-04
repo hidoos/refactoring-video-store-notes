5b6dd406f9333cd9dea7c4e3d443a6420b111225
hidoos <sk8huaban@gmail.com>
2018-07-05 00:30:03 +0800

    Data Transformation: touching base version of video store

---
+++ DataTransformation/video-store.js
@@ -0,0 +1,72 @@
+/**
+ * using top-level functions
+ */
+const {movies} = require('../movies');
+const {customer} = require('../customer');
+
+function movieFor(rental, movies) {
+  return movies[rental.movieID];
+}
+
+function amountFor(r, movies) {
+  let result = 0;
+
+  // determine amount for each movie
+  switch (movieFor(r, movies).code) {
+    case 'regular':
+      result = 2;
+      if (r.days > 2) {
+        result += (r.days - 2) * 1.5;
+      }
+      break;
+    case 'new':
+      result = r.days * 3;
+      break;
+    case 'childrens':
+      result = 1.5;
+      if (r.days > 3) {
+        result += (r.days - 3) * 1.5;
+      }
+      break;
+  }
+  return result;
+}
+
+function totalAmount(customer, movies) {
+  let result = 0;
+  for (let r of customer.rentals) {
+    result += amountFor(r, movies);
+  }
+  return result;
+}
+
+function totalFrequentRenterPoints(customer, movies) {
+  let result = 0;
+  for (let r of customer.rentals) {
+    result += frequentRenterPointsFor(r, movies);
+  }
+  return result;
+}
+
+function frequentRenterPointsFor(r, movies) {
+  // add frequent renter points
+  // add bonus for a two day new release rental
+  return (movieFor(r, movies).code === 'new' && r.days > 2) ? 2 : 1;
+}
+
+function statement(customer, movies) {
+  let result = `Rental Record for ${customer.name}\n`;
+
+  for (let r of customer.rentals) {
+    result += `\t${movieFor(r, movies).title}\t${amountFor(r, movies)}\n`;
+  }
+
+  // add footer lines
+  result += `Amount owed is ${totalAmount(customer, movies)}\n`;
+  result += `You earned ${totalFrequentRenterPoints(customer, movies)} frequent renter points\n`;
+
+  return result;
+}
+
+console.log(statement(customer, movies));
+
