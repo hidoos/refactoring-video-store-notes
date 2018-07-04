a21fb10cad51067a6ff459c95884c34e1375460d
hidoos <sk8huaban@gmail.com>
2018-07-04 11:28:31 +0800

    parameter dispatch: 新增一个todo，稍后使用函数 curry 的方式写一个包裹函数，返回新的函数，来包装掉一些信息，调用只用传 format

--- store-using-top.js
+++ store-using-top.js
@@ -8,32 +8,62 @@ function movieFor(rental, movies) {
   return movies[rental.movieID];
 }
 
-function statement(customer, movies) {
+function amountFor(r, movies) {
+  let result = 0;
+
+  // determine amount for each movie
+  switch (movieFor(r, movies).code) {
+    case 'regular':
+      result = 2;
+      if (r.days > 2) {
+        result += (r.days - 2) * 1.5;
+      }
+      break;
+    case 'new':
+      result = r.days * 3;
+      break;
+    case 'childrens':
+      result = 1.5;
+      if (r.days > 3) {
+        result += (r.days - 3) * 1.5;
+      }
+      break;
+  }
+  return result;
+}
+
+function totalAmount(customer, movies) {
+  let result = 0;
+  for (let r of customer.rentals) {
+    result += amountFor(r, movies);
+  }
+  return result;
+}
+
+function totalFrequentRenterPoints(customer, movies) {
+  let result = 0;
+  for (let r of customer.rentals) {
+    result += frequentRenterPointsFor(r, movies);
+  }
+  return result;
+}
+
+function frequentRenterPointsFor(r, movies) {
+  // add frequent renter points
+  // add bonus for a two day new release rental
+  return (movieFor(r, movies).code === 'new' && r.days > 2) ? 2 : 1;
+}
+
+function textStatement(customer, movies) {
   let result = `Rental Record for ${customer.name}\n`;
 
   for (let r of customer.rentals) {
-    result += `\t${movieFor(r, movies).title}\t${amountFor(r)}\n`;
+    result += `\t${movieFor(r, movies).title}\t${amountFor(r, movies)}\n`;
   }
 
   // add footer lines
-  result += `Amount owed is ${totalAmount()}\n`;
-  result += `You earned ${totalFrequentRenterPoints()} frequent renter points\n`;
-
-  function totalAmount() {
-    let result = 0;
-    for (let r of customer.rentals) {
-      result += amountFor(r);
-    }
-    return result;
-  }
-
-  function totalFrequentRenterPoints() {
-    let result = 0;
-    for (let r of customer.rentals) {
-      result += frequentRenterPointsFor(r);
-    }
-    return result;
-  }
+  result += `Amount owed is ${totalAmount(customer, movies)}\n`;
+  result += `You earned ${totalFrequentRenterPoints(customer, movies)} frequent renter points\n`;
 
   /**
    * another way is using pipeline methods
@@ -50,55 +80,33 @@ function statement(customer, movies) {
     }
    */
 
-  function amountFor(r) {
-    let result = 0;
-
-    // determine amount for each movie
-    switch (movieFor(r, movies).code) {
-      case 'regular':
-        result = 2;
-        if (r.days > 2) {
-          result += (r.days - 2) * 1.5;
-        }
-        break;
-      case 'new':
-        result = r.days * 3;
-        break;
-      case 'childrens':
-        result = 1.5;
-        if (r.days > 3) {
-          result += (r.days - 3) * 1.5;
-        }
-        break;
-    }
-    return result;
-  }
-
-  function frequentRenterPointsFor(r) {
-    // add frequent renter points
-    // add bonus for a two day new release rental
-    return (movieFor(r, movies).code === 'new' && r.days > 2) ? 2 : 1;
-  }
-
   return result;
 }
 
-console.log(statement(customer, movies));
+function htmlStatement(customer, movies) {
+  let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n`;
+  result += "<table>\n";
+  for (let r of customer.rentals) {
+    result += `  <tr><td>${movieFor(r, movies).title}</td><td>${amountFor(r, movies)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${totalAmount(customer, movies)}</em></p>\n`;
+  result += `<p>You earned <em>${totalFrequentRenterPoints(customer, movies)}</em> frequent renter points</p>\n`;
+  return result;
+}
 
-/* statement method output
-  Rental Record for martin
-    Ran 3.5
-    Trois Couleurs: Bleu 2
-  Amount owed is 5.5
-  You earned 2 frequent renter points
-*/
+function statement(customer, movies, format = '') {
+  const dispatchTable = {
+    text: textStatement,
+    html: htmlStatement
+  };
+  if (undefined === dispatchTable[format]) throw new Error(`unknown statement for ${format}`)
+  return dispatchTable[format].call(null, customer, movies);
+}
 
-/* hope output
-  <h1>Rental Record for <em>martin</em></h1>
-  <table>
-    <tr><td>Ran</td><td>3.5</td></tr>
-    <tr><td>Trois Couleurs: Bleu</td><td>2</td></tr>
-  </table>
-  <p>Amount owed is <em>5.5</em></p>
-  <p>You earned <em>2</em> frequent renter points</p>
- */
+// todo 使用函数 curry 去包装 statement，返回一个新的函数，只用传 format 参数来调用
+console.log(statement(customer, movies, 'text'));
+
+// console.log(htmlStatement(customer, movies));
+// console.log(statement(customer, movies));
+
